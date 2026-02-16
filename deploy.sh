087b55b6e6289bb90ff998202e3cb8643a6b39df
liavna <14238113+liavna@users.noreply.github.com>
2026-02-16 17:59:56 +0000

    fix: Package Helm chart before deployment
    - Updated `deploy.sh` to package the Helm chart into a `.tgz` file before attempting deployment.
    - This addresses the "Could not find the matching uploaded chart" error by ensuring the expected artifact is available.
    - Updated `deploy.sh` to use the packaged `.tgz` file for `helm upgrade --install`.

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,16 @@ set -e
 
 NAMESPACE="speech-diarization"
 RELEASE_NAME="speech-diarization"
+CHART_NAME="speech-diarization-ui"
+VERSION="1.0.0"
 TIMEOUT="15m0s"
+
+echo "Packaging ${CHART_NAME} version ${VERSION}..."
+helm package .
 
 echo "Deploying ${RELEASE_NAME} to namespace ${NAMESPACE}..."
 
-helm upgrade --install ${RELEASE_NAME} . \
+helm upgrade --install ${RELEASE_NAME} ${CHART_NAME}-${VERSION}.tgz \
     --namespace ${NAMESPACE} \
     --create-namespace \
     --atomic \
